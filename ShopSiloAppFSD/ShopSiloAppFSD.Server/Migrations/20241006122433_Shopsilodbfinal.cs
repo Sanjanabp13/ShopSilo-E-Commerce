d7639eb0867230e0942f9c31dbbeb5b9693e0821
Sanjanabp13 <sanjana.biminiprakashbabu@gmail.com>
2024-10-07 19:44:29 +0530

    shopsilo commit

---
+++ ShopSiloAppFSD/ShopSiloAppFSD.Server/Migrations/20241006122433_Shopsilodbfinal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopSiloAppFSD.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Shopsilodbfinal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
